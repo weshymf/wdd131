fda210704a73126eaf68aca7e270db92c0714cfd
weshymf <148005562+weshymf@users.noreply.github.com>
2024-06-23 00:00:35 -0300

    Add files via upload

---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+// temples.js
+
+// Update last modified date in footer
+const lastModifiedElement = document.getElementById('lastModified');
+const lastModifiedDate = document.lastModified;
+lastModifiedElement.textContent = lastModifiedDate;
