440311be1988d102d45e1ba2a44d3aca8e276ae6
weshymf <148005562+weshymf@users.noreply.github.com>
2024-07-13 21:15:16 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const products = [
+        { id: "fc-1888", name: "flux capacitor", avgRating: 4.5 },
+        { id: "fc-2050", name: "power laces", avgRating: 4.7 },
+        { id: "fs-1987", name: "time circuits", avgRating: 3.5 },
+        { id: "ac-2000", name: "low voltage reactor", avgRating: 3.9 },
+        { id: "jj-1969", name: "warp equalizer", avgRating: 5.0 }
+    ];
+
+    const productSelect = document.getElementById("product-name");
+
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.name;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
+    });
+});
